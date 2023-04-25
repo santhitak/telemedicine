d8e41fdfb12abd18a07139cfe978a486420ca081
santhitak <santhita.sk@gmail.com>
2023-04-25 12:39:41 +0700

    fix: import without exp def

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import StoreProvider from "@/lib/StoreProvider";
-import { Nav } from "./components";
+import Nav from "./components/Nav";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
